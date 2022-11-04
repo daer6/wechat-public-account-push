9804a87b49e4370653e1de63b2fcb7d4b0a8b4ae
daer6 <99600329+daer6@users.noreply.github.com>
2022-11-04 10:52:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,11 +63,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '小欣欣',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oLBnL6RhzkhoEkaXoILXV2WQXEGw',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Ic1OQ-uo3Ns3TgXmc7QiIp_ggevIHAN2K6LBsZZtbKU',
       // 所在省份
       province: '江苏',
       // 所在城市
